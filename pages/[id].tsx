33adf9de1b45a64d6615e6d87da9c5e10eb3982c
MartDL <martlord81@hotmail.com>
2023-08-07 15:59:26 +0100

    single movie route / actors cards

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -16,12 +16,49 @@ import { basicFetch } from "@/api/fetchFuctions";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import type { Movie, Credits, Crew, Cast } from "../api/types";
 
-const Movie: NextPage = () => (
+type Props = {
+  movie: Movie;
+  directors: Crew[];
+  cast: Cast[];
+};
+
+const Movie: NextPage<Props> = ({ movie, directors, cast }: Props) => (
   <main>
     <Header />
-    <MovieInfo />
-    <Grid>
-      <Card />
+    <Breadcrumb title={movie.original_title} />
+    <MovieInfo
+      thumbUrl={
+        movie.poster_path
+          ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+          : "/no_image.jpg"
+      }
+      rating={movie.vote_average}
+      year={movie.release_date.split("-")[0]}
+      backgroundImgUrl={
+        movie.backdrop_path
+          ? IMAGE_BASE_URL + BACKDROP_SIZE + movie.backdrop_path
+          : "/o_image.jpg"
+      }
+      title={movie.original_title}
+      summary={movie.overview}
+      directors={directors}
+      time={movie.runtime}
+      budget={movie.budger}
+      revenue={movie.revenue}
+    />
+    <Grid className="p-4 max-w-7xl m-auto" title="Actors">
+      {cast.map((actor) => (
+        <Card
+          key={actor.credit_id}
+          imgUrl={
+            actor.profile_path
+              ? IMAGE_BASE_URL + POSTER_SIZE + actor.profile_path
+              : "/no_image.jpg"
+          }
+          title={actor.name}
+          subtitle={actor.character}
+        />
+      ))}
     </Grid>
   </main>
 );
@@ -30,4 +67,28 @@ export default Movie;
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context.params?.id as string;
+
+  const movieEndpoint: string = movieUrl(id);
+  const creditsEndpoint: string = creditsUrl(id);
+
+  const movie = await basicFetch<Movie>(movieEndpoint);
+  const credits = await basicFetch<Credits>(creditsEndpoint);
+
+  const directors = credits.crew.filter((member) => member.job === "Director");
+
+  return {
+    props: {
+      movie,
+      directors,
+      cast: credits.cast,
+    },
+    revalidate: 60 * 60 * 24, // rebuild page every 24 hours
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,9 @@ module.exports = {
     fontFamily: {
       raleway: ["Raleway", "sans-serif"],
     },
+    minHeight: {
+      128: "40rem",
+    },
   },
   plugins: [],
 };
